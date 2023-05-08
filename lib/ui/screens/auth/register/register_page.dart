6cf2ecb65b65d4947f7faad8caf4b4d01356ba0d
halimbimantara <mhalimbimantara@gmail.com>
2023-05-08 09:05:17 +0700

    - Ui Component   - Register

--- lib/ui/screens/auth/register/register_page.dart
+++ lib/ui/screens/auth/register/register_page.dart
@@ -4,7 +4,6 @@ import 'package:vascommtest/shared/themes.dart';
 import 'package:vascommtest/shared/ui_component/kedittext_cs_widget.dart';
 import 'package:vascommtest/ui/screens/auth/widget/header_userauth_page.dart';
 import 'package:vascommtest/utils/localization/rstring.dart';
-import 'package:vascommtest/utils/utils.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -20,92 +19,60 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: CustomScrollView(
-          children: <Widget>[
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             const HeaderUserAuth(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                Text(
-                  RString.labelFirstName,
-                  style: kTextTitleLabel,
-                ),
-                Text(
-                  RString.labelLastName,
-                  style: kTextTitleLabel,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                TextField()
-              //   CommonTextFieldSimple(
-              //     colorText: greySubTitle,
-              //     hint: RString.labelFirstName,
-              //     controller: firstNameController,
-              //     enable: true,
-              //     colors: Colors.white,
-              //     boxShadow: [
-              //       BoxShadow(
-              //         color: kWhiteBG.withOpacity(0.5),
-              //         spreadRadius: 1,
-              //         blurRadius: 5,
-              //         offset: const Offset(0, 3),
-              //       ),
-              //     ],
-              //   ),
-              //   CommonTextFieldSimple(
-              //     colorText: greySubTitle,
-              //     hint: RString.labelFirstName,
-              //     controller: firstNameController,
-              //     enable: true,
-              //     colors: Colors.white,
-              //     boxShadow: [
-              //       BoxShadow(
-              //         color: kWhiteBG.withOpacity(0.5),
-              //         spreadRadius: 1,
-              //         blurRadius: 5,
-              //         offset: const Offset(0, 3),
-              //       ),
-              //     ],
-              //   ),
-              ],
-            )
-          ],
+            inputSection()],
         ),
       ),
     );
   }
 }
 
-Widget registerScreen() => Container(
-      height: AppUtils.height / 1.2,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          CommonTextFieldSimple(
-            colorText: greySubTitle,
-            hint: RString.labelFirstName,
-            controller: firstNameController,
-            enable: true,
-            colors: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: kWhiteBG.withOpacity(0.5),
-                spreadRadius: 1,
-                blurRadius: 5,
-                offset: const Offset(0, 3),
+Widget inputSection() => Container(
+      margin: const EdgeInsets.only(top: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              Text(
+                RString.labelFirstName,
+                style: kTextTitleLabel,
+              ),
+              Text(
+                RString.labelLastName,
+                style: kTextTitleLabel,
               ),
             ],
           ),
-          const Text(
-            RString.labelLastName,
-            style: kTextTitleLabel,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              CommonTextFieldSimple(
+                colorText: greySubTitle,
+                hint: RString.labelFirstName,
+                controller: firstNameController,
+                enable: true,
+                colors: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: kWhiteBG.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              const Text(
+                RString.labelLastName,
+                style: kTextTitleLabel,
+              ),
+            ],
           ),
         ],
       ),
